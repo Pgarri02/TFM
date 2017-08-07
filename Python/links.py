13b61d769c6ffd4034aa006d5aa6f72653d94bdc
Pgarri02 <patricia@MacBook-Air-de-Patricia.local>
2017-08-07 17:56:30 +0200

    Código en Python - generar links final

--- Python/links.py
+++ Python/links.py
@@ -4,25 +4,9 @@ Created on Mon Jun 19 11:19:39 2017
 
 """
 
-def generar_links_plano():
-    f = open('texto.txt', 'r')
-    f2 = open('texto_links.txt', 'w')
-    ls = f.readlines()
-    texto = ""
-    for linea in ls:
-        palabras = linea.split()
-        for pal in palabras:
-            if pal[-1] == ",":
-                pal = pal[:-1]
-                texto = texto + "<a href=\"http://logeion.uchicago.edu/index.html#" + pal + "\">" +  pal + "</a>" + ", "
-            else:
-                texto = texto + "<a href=\"http://logeion.uchicago.edu/index.html#" + pal + "\">" +  pal + "</a>" + " "
-        texto = texto + "\n"
-    f2.write(texto)
-    f.close()
-    f2.close()
 
-def generar_links_htmlv2():
+
+def generar_links_final():
     f = open('teognisgriego.html', 'r', encoding='utf-8')
     f2 = open('teognisgriego_links.html', 'w', encoding='utf-8')   
     ls = f.readlines()
@@ -57,34 +41,4 @@ def generar_links_htmlv2():
     f.close()
     f2.close()         
 
-def generar_links_html():
-    f = open('teognisgriego.html', 'r')
-    f2 = open('texto_links.txt', 'w')
-    ls = f.readlines()
-    for linea in ls:
-        mi_pal = ""
-        link = False
-        for letra in linea:
-            if letra == "<":
-                link = False
-                if mi_pal != "":
-                    
-                    mi_pal = "<a href=\"http://logeion.uchicago.edu/index.html#" + mi_pal + "\">" +  mi_pal + "</a>"
-                    f2.write(mi_pal)
-                mi_pal = ""
-                f2.write(letra)
-            elif letra == " " or letra == "," or letra == "." or letra == "\n":
-                if mi_pal != "":
-                    mi_pal = "<a href=\"http://logeion.uchicago.edu/index.html#" + mi_pal + "\">" +  mi_pal + "</a>"
-                    f2.write(mi_pal)
-                mi_pal = ""
-                f2.write(letra)
-            elif link:
-                mi_pal += letra
-            elif letra == ">":
-                link = True
-                f2.write(letra)
-            else:
-                f2.write(letra)
-    f.close()
-    f2.close()
+d
